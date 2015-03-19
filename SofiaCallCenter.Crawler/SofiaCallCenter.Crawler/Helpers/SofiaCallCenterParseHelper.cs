9ce5f1a6bd27b33ed6d35a1098e0c2f884f67706
Todor Mihaylov <tbmihailov@gmail.com>
2015-03-19 13:58:51 +0200

    Added SofiaCallCenterParseHelper for common parse operations

---
+++ SofiaCallCenter.Crawler/SofiaCallCenter.Crawler/Helpers/SofiaCallCenterParseHelper.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace SofiaCallCenter.Crawler.Helpers
+{
+    public class SofiaCallCenterParseHelper
+    {
+        public static Signal ParseSingleSignalData(int signalId, HtmlAgilityPack.HtmlDocument doc)
+        {
+            if ((doc.DocumentNode.SelectNodes("//p[@id='statusIndicator']") == null)
+                || (doc.DocumentNode.SelectNodes("//p[@id='statusIndicator']").Count == 0))
+            {
+                throw new SignalHtmlStructureNotDetectedException(signalId);
+            }
+
+            //get sections from html
+            var docNodes = doc.DocumentNode.SelectNodes("//section[@class]");
+
+            //get number and category
+            var nodeSectionWithCategory = docNodes.FirstOrDefault();
+            var signalStatus = nodeSectionWithCategory.SelectSingleNode("//p[@id='statusIndicator']").InnerText;
+            var signalDocNumberInfo = nodeSectionWithCategory.SelectSingleNode("//h3").InnerText;
+            var signalCategoryFull = nodeSectionWithCategory.SelectSingleNode("//h4").InnerText;
+
+            //get content
+            var nodeSectionContent = docNodes[1];
+            var signalTitle = nodeSectionWithCategory.SelectSingleNode("//div/blockquote").InnerText;
+            var signalDescription = nodeSectionWithCategory.SelectSingleNode("//fieldset/div[@class='display-field']").InnerText;
+
+            var signalLocationText = doc.DocumentNode.SelectSingleNode("//fieldset//label[@for='LOCATION']").ParentNode.ParentNode.InnerText;
+            decimal lat = 0;
+            decimal lon = 0;
+            ExtractLatLon(signalLocationText, ref lat, ref lon);
+
+            Signal signal = new Signal();
+            signal.Id = signalId;
+            signal.DocNumberInfo = signalDocNumberInfo;
+            signal.Title = signalTitle;
+            signal.Description = signalDescription;
+            signal.Status = signalStatus;
+            signal.CategoryFull = signalCategoryFull;
+            signal.LocationLat = lat;
+            signal.LocationLon = lon;
+
+            string[] categoryLevels = signalCategoryFull.Split('/');
+            signal.CategoryLevel1 = categoryLevels[0];
+            signal.CategoryLevel2 = categoryLevels.Length > 1 ? categoryLevels[1] : string.Empty;
+            return signal;
+        }
+
+        private static void ExtractLatLon(string signalLocationText, ref decimal lat, ref decimal lon)
+        {
+            var match = Regex.Match(signalLocationText, @"\[([\d\.]+)\,([\d\.]+)\]");
+            if (match.Success && match.Groups.Count > 2)
+            {
+                lat = Decimal.Parse(match.Groups[1].Value);
+                lon = Decimal.Parse(match.Groups[2].Value);
+            }
+        }
+    }
+}
--- SofiaCallCenter.Crawler/SofiaCallCenter.Crawler/Program.cs
+++ SofiaCallCenter.Crawler/SofiaCallCenter.Crawler/Program.cs
@@ -21,21 +21,22 @@ namespace SofiaCallCenter.Crawler
             {
                 signalFrom = int.Parse(args[0]);
             }
-            
+
             if (args.Length >= 2)
             {
                 signalTo = int.Parse(args[1]);
             }
 
-            string outputFile = string.Format("CCS_Signals-{0}-{1}.csv",signalFrom, signalTo);
+            string outputFile = string.Format("CCS_Signals-{0}-{1}.csv", signalFrom, signalTo);
             if (args.Length >= 3)
             {
                 outputFile = args[2];
             }
-            
+
             List<Signal> crawledSignals = CrawlSignalsFromCallSofiaBg(signalFrom, signalTo, 10);
 
-            using(StreamWriter sw = new StreamWriter(outputFile)){
+            using (StreamWriter sw = new StreamWriter(outputFile))
+            {
                 CsvWriter writer = new CsvWriter(sw);
                 foreach (var signal in crawledSignals)
                 {
@@ -84,39 +85,11 @@ namespace SofiaCallCenter.Crawler
             HtmlAgilityPack.HtmlDocument doc = new HtmlAgilityPack.HtmlDocument();
             doc.Load(webClient.OpenRead(signalUrl), Encoding.UTF8);
 
-            if ((doc.DocumentNode.SelectNodes("//p[@id='statusIndicator']") == null)
-                || (doc.DocumentNode.SelectNodes("//p[@id='statusIndicator']").Count == 0))
-            {
-                throw new SignalHtmlStructureNotDetectedException(signalId);
-            }
-
-            //get sections from html
-            var docNodes = doc.DocumentNode.SelectNodes("//section[@class]");
-
-            //get number and category
-            var nodeSectionWithCategory = docNodes.FirstOrDefault();
-            var signalStatus = nodeSectionWithCategory.SelectSingleNode("//p[@id='statusIndicator']").InnerText;
-            var signalDocNumberInfo = nodeSectionWithCategory.SelectSingleNode("//h3").InnerText;
-            var signalCategoryFull = nodeSectionWithCategory.SelectSingleNode("//h4").InnerText;
-
-            //get content
-            var nodeSectionContent = docNodes[1];
-            var signalTitle = nodeSectionWithCategory.SelectSingleNode("//div/blockquote").InnerText;
-            var signalDescription = nodeSectionWithCategory.SelectSingleNode("//fieldset/div[@class='display-field']").InnerText;
-
-            Signal signal = new Signal();
-            signal.Id = signalId;
-            signal.DocNumberInfo = signalDocNumberInfo;
-            signal.Title = signalTitle;
-            signal.Description = signalDescription;
-            signal.Status = signalStatus;
-            signal.CategoryFull = signalCategoryFull;
-
-            string[] categoryLevels = signalCategoryFull.Split('/');
-            signal.CategoryLevel1 = categoryLevels[0];
-            signal.CategoryLevel2 = categoryLevels.Length > 1 ? categoryLevels[1] : string.Empty;
+            Signal signal = Helpers.SofiaCallCenterParseHelper.ParseSingleSignalData(signalId, doc);
 
             return signal;
         }
+
+        
     }
 }
